e1ddea13955d114dce50c565366d2febf9525aa7
Sumanvith <dellbilla@gmail.com>
2024-06-27 22:00:09 +0530

    EP15 2/7

--- src/utils/appStore.js
+++ src/utils/appStore.js
@@ -1,6 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
 import useReducer from "./userSlice";
+import moviesReducer from "./movieSlice";
 
-const appStore = configureStore({ reducer: { user: useReducer } });
+const appStore = configureStore({
+  reducer: { user: useReducer, movies: moviesReducer },
+});
 
 export default appStore;
